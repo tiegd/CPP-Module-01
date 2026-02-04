f9754f5dcd49b9ffc37625d1d0079f84ea3c6d62
Gautier Ducurtil <gaducurt@student.42.fr>
2026-02-04 11:30:25 +0100

    ex04 work correctly

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 16:43:54 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/02/03 19:56:46 by gaducurt         ###   ########.fr       */
+/*   Updated: 2026/02/04 11:28:20 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ void	replace()
 
 int	main(int ac, char **av)
 {
-	(void)ac;
 	if (ac == 4)
 	{
-		std::string const 	s1 = av[2];
-		std::string 	s2 = av[3];
-		std::string		line;
-		std::ifstream	infile;
+		std::string const 		s1 = av[2];
+		std::string 			s2 = av[3];
+		std::string				line;
+		std::ifstream			infile;
+		std::string::size_type	n;
 
 		infile.open(av[1]);
 		std::ofstream	outfile;
@@ -37,26 +37,23 @@ int	main(int ac, char **av)
 
 		outfile.open(av[1]);
 		if (!outfile.is_open())
-		{
-			std::cout << "the file isn't open" << std::endl;
 			return (1);
-		}
 		if (infile.is_open())
 		{
 			while (std::getline(infile, line, '\0'))
 			{
-				// line.erase(line.find(s1));
-				line.find(s1);
-				int n = line.find('\n');
-				// std::cout << line.find('\n') << std::endl;
-				// n = line.find('\n', n);
-				// std::cout << line.find('\n', n + 1) << std::endl;
-				outfile << line << std::endl;
+				n = line.find(s1);
+				while (n <= std::string::npos)
+				{
+					if (n == std::string::npos)
+						break;
+					line.erase(n, s1.length());
+					line.insert(n, s2);
+					n = line.find(s1);
+				}
 			}
-				// std::cout << line << std::endl;
+			outfile << line << std::endl;
 		}
-		std::cout << "the file was correctly opened" << std::endl;
-		// myfile.close();
 	}
 	return (0);
 }
