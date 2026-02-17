637371b2af41037455a634e56fe7fbc4e81407ce
Gautier Ducurtil <gaducurt@student.42.fr>
2026-02-17 10:44:42 +0100

    modif HumanA::attack()

--- ex03/HumanA.cpp
+++ ex03/HumanA.cpp
@@ -6,7 +6,7 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 14:57:50 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/02/03 16:39:08 by gaducurt         ###   ########.fr       */
+/*   Updated: 2026/02/17 10:39:34 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ HumanA::HumanA(std::string name, Weapon &w): weapon(w)
 
 void	HumanA::attack()
 {
-	std::cout << name << " attacks with their " << weapon.getType() << std::endl;
+	std::cout << this->name << " attacks with their " << weapon.getType() << std::endl;
 }
 
 HumanA::~HumanA()
